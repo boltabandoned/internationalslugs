9da74fd8a1e0a3e71d9f9255ce32ee750d02c320
Alan Smithee <alan.smithee@example.com>
2016-08-09 13:19:39 +0200

    Bolt v3 update

--- init.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-use Bolt\Extension\boltabandoned\internationalslugs\Extension;
-
-$app['extensions']->register(new Extension($app));
---
+++ src/InternationalSlugsExtension.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Bolt\Extension\boltabandoned\InternationalSlugs;
+
+use Bolt\Extension\SimpleExtension;
+use Cocur\Slugify\Slugify;
+use Bolt\Application;
+
+/**
+ * InternationalSlugs extension class.
+ *
+ * @author Alan Smithee <alan.smithee@example.com>
+ */
+class InternationalSlugsExtension extends SimpleExtension
+{
+    /**
+     * @inheritdoc
+     */
+    protected function registerServices(Application $app)
+    {
+        $config = $this->getConfig();
+        $app['slugify'] = $app->share(
+            function ($app) use ($config) {
+                if (!isset($config['regexp'])) {
+                    $config['regexp'] = '/([^A-Za-z0-9]|-)+/';
+                }
+
+                if (!isset($config['lowercase'])) {
+                    $config['lowercase'] = true;
+                }
+
+                $slugify = new Slugify(
+                    $config['regexp'],
+                    array('lowercase' => $config['lowercase'])
+                );
+
+                foreach($config['rules'] as $key => $value){
+                    $slugify->addRule($key, $value);
+                }
+
+                return $slugify;
+            }
+        );
+    }
+}
